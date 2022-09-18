f5f4ca6761eff8b64e5ae454772c6e84db3208f8
Elber Funez <elberfunez@gmail.com>
2022-09-18 19:47:51 -0400

    adding files

---
+++ CoffeeShop/CoffeeShopMVCContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShop
+{
+    public partial class CoffeeShopMVCContext : DbContext
+    {
+        public CoffeeShopMVCContext()
+        {
+        }
+
+        public CoffeeShopMVCContext(DbContextOptions<CoffeeShopMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=CoffeeShopMVC;User Id=sa;Password=Pa$$wor4d;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+
+                entity.Property(e => e.LastName).HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private CoffeeShopMVCContext context = new CoffeeShopMVCContext();
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    // methods that return the view
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    // methods that function with the DB
+
+    public IActionResult AddUserToDb(User user)
+    {
+        context.Users.Add(user);
+        context.SaveChanges();
+        return RedirectToAction("ThankYouSignUp", user); // go to view of all users 
+    }
+
+    public IActionResult ThankYouSignUp(User user)
+    {
+        List<User> newUser = new List<User>()
+        {
+            user
+        };
+        return View(newUser);
+
+    }
+    public IActionResult ShowAllUsers()
+    {
+        List<User> users = context.Users.ToList();
+        return View(users);
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
